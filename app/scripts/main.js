3aae0088da73b606932b520a47cf514c11e2c5ff
Jennifer Siepert <jpsiepert@yahoo.com>
2014-09-24 01:10:49 -0600

    refactors css

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,22 +2,28 @@ $(document).ready(function(){
 
 $('#newTaskForm').hide();
 
+
 var listo = [];
 var Task = function(task) {
     this.task = task;
     this.id = 'new';
 }
-var addTask = function(task) {
-	if(task) {
-		task = new Task(task);
-		listo.push(task);
-		$('#newItemInput').val('');
-        $('#newList').append('<a href="#finish" class="" id="item"><li class="list-group-item">' + task.task + '<span class="arrow pull-right"><i class="glyphicon glyphicon-arrow-right"></span></li></a>');
-	}
-	$('#newTaskForm,  #newListItem').fadeToggle('fast', 'linear');
-	save();
-};
 
+
+
+var addTask = function(task) {
+    if(task) {
+        task = new Task(task);
+        listo.push(task);
+        save();
+        $('#newItemInput').val('');
+
+        $('#newList').append('<a href="#finish" class="" id="item"><li class="list-group-item">' + task.task + '<span class="arrow pull-right"><i class="glyphicon glyphicon-arrow-right"></span></li></a>');
+
+    }
+    $('#newTaskForm,  #newListItem').fadeToggle('fast', 'linear');
+
+};
 var advanceTask = function(task) {
 	var modified = task.innerText.trim();
 	for (var i = 0; i < listo.length; i++) {
@@ -31,7 +37,8 @@ var advanceTask = function(task) {
 			else {
 				listo.splice(i, 1);
 			}
-			
+			save();
+      break;
 		}
 		 
 	}
@@ -42,16 +49,19 @@ $('#saveNewItem').on('click', function (e) {
     e.preventDefault();
     var task = $('#newItemInput').val().trim();
     addTask(task);
+    $('#hideit').show();
 });
 
 //open form
 $('#newListItem').on('click', function () {
     $('#newTaskForm,  #newListItem').fadeToggle('fast', 'linear');
+    $('#hideit').hide();
 });
 //closes form
 $('#cancel').on('click', function (e) {
     e.preventDefault();
     $('#newTaskForm,  #newListItem').fadeToggle('fast', 'linear');
+    $('#hideit').show();
 });
 
 $(document).on('click', '#item', function(e) {
